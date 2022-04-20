a71c31c67581b94ea80945f9596abd5165230cd8
Osman Sahan <osahan@gmail.com>
2022-04-20 15:15:36 +0300

    20.04-2

---
+++ src/GeometriHesaplamaYontemi/Yontem2/Kare.java
@@ -0,0 +1,39 @@
+package GeometriHesaplamaYontemi.Yontem2;
+
+import java.util.Scanner;
+
+import static GeometrikHesaplama.src.packageSekil.sekil.*;
+
+public class Kare {
+
+
+    protected static void kareIslemleri() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lutfen karenin  bir kenarinin olcusunu cm olarak giriniz : ");
+        kenarUzunlugu = scan.nextDouble();
+
+
+    }
+
+    protected static void kareCevreHesapla() {
+        kareIslemleri();
+        double kareCevresi = kenarUzunlugu * 4;
+        System.out.println("Olculerini verdiginiz karenin cevresi " + kareCevresi + " " + cevreMetrikBirim + "'dir");
+    }
+
+    protected static void kareAlanHesapla() {
+        kareIslemleri();
+        double kareAlani = kenarUzunlugu * kenarUzunlugu;
+        System.out.println("Olculerini verdiginiz karenin alani: " + kareAlani + " " + alanMetrikBirim + "'dir");
+
+
+    }
+
+
+}
+
+
+
+
+
